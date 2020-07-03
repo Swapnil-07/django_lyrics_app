8bc31914b506e13d53e23a43199453e82fcf7637
Swapnil-07 <5993157+Swapnil-07@users.noreply.github.com>
2020-07-03 23:54:34 +0530

    pipline setup

--- lyrics_app/home/urls.py
+++ lyrics_app/home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='Home'),
+    path('album/<int:al_id>', views.album, name='Album'),
+    path('song/<int:s_id>', views.song, name='Song'),
+    path('artist/<int:ar_id>', views.artist, name='Artist'),
 ]
--- lyrics_app/home/views.py
+++ lyrics_app/home/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, "base.htm")
+    return render(request, "index.htm")
+
+def album(request, al_id):
+    albums = {1:'Rang De Basanti', 2:'Friends', 3:'Bahubali'}
+    return render(request, "album.htm", {'albumName':albums[al_id]})
+
+def song(request, s_id):
+    songs = {1:'Namo Namo Hai Shankara', 2:'Kal Ho Na Ho', 3:'Hai Apna Dil To Aawara'}
+    return render(request, "song.htm", {'songName':songs[s_id]})
+
+def artist(request, ar_id):
+    artists = {1:'Arijit Singh', 2:'Shreya Ghoshal', 3:'Sonu Nigam'}
+    return render(request, "artist.htm", {'artistName':artists[ar_id]})
+
