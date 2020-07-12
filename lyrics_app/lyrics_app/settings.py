26707dfe77090428d696112e9ace0487eceda174
vaibhav1510 <vaibhav@chargebee.com>
2020-07-12 16:20:21 +0530

    change

--- lyrics_app/lyrics_app/settings.py
+++ lyrics_app/lyrics_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'pm+jk_gc(e9*2#tl7!=bo3nh7+b%g+h&5nod804r#))br8&&%w'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*.herokuapp.com'
+    'django-lyrics-app.herokuapp.com'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
