4d8c765b2fdda1de14f0d9172c9b28dc135fc4c2
vaibhav1510 <vaibhav@chargebee.com>
2020-07-12 15:39:39 +0530

    change

--- lyrics_app/lyrics_app/settings.py
+++ lyrics_app/lyrics_app/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'pm+jk_gc(e9*2#tl7!=bo3nh7+b%g+h&5nod804r#))br8&&%w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*.herokuapp.com'
+]
 
 
 # Application definition
