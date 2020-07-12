a114ea129f454338fdf52e93081c8cb7bfbff897
vaibhav1510 <vaibhav@chargebee.com>
2020-07-12 16:01:01 +0530

    change

--- lyrics_app/lyrics_app/settings.py
+++ lyrics_app/lyrics_app/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pm+jk_gc(e9*2#tl7!=bo3nh7+b%g+h&5nod804r#))br8&&%w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = [
