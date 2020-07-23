01f3acf1e1bec5d974b45c45133f75cd4e9e4d81
vaibhav1510 <vaibhav@chargebee.com>
2020-07-23 17:30:35 +0530

    local db

--- lyrics_app/lyrics_app/settings/dev.py
+++ lyrics_app/lyrics_app/settings/dev.py
@@ -1,6 +1,30 @@
+import os
+from . import base
+
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1'
 ]
 
 SECURE_SSL_REDIRECT = False
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(base.BASE_DIR, 'db_dev.sqlite3'),
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
